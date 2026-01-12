bfaeeb91fb833163521734aa5e46e5039c9ed7b9
Lorena Gallart <correo-usuario-lorena@gmail.com>
2026-01-12 19:19:32 +0100

    Primero cambio Lorena

--- TablaDel12.java
+++ TablaDel12.java
@@ -7,5 +7,6 @@ public class TablaDel12
             System.out.println("12 * "+ i + " = " + 12 * i + "\n");
         }
         System.out.println("Se ha imprimido por pantalla la tabla del 12");
+        System.out.println("En el siguiente fichero estará la tabla del 13");
     }
 }
